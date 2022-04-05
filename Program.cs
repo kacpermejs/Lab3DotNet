022421f3fa12caa45026ef5cbcceeab19624c793
Student 254014 <254014@student.pwr.edu.pl>
2022-04-05 19:32:03 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(Welcome);
+            thread.Start();
+            Console.Read();
+        }
+        static void Welcome()
+        {
+            Console.WriteLine("Hallo!");
+        }
+    }
+}
